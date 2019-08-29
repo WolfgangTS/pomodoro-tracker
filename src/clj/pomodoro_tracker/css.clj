e903bd78325ec730b7ea47f02a29d0b1680adda4
Emily Martins <emily.flakeheart@gmail.com>
2019-08-29 21:45:30 +0200

    fix: removed off-screen progress bar

--- src/clj/pomodoro_tracker/css.clj
+++ src/clj/pomodoro_tracker/css.clj
@@ -5,6 +5,5 @@
   [:html :body {:background-color "hsl(0, 0%, 96%)"}]
   [:.big-number {:font-size "70px"}]
   [:.timer-app {}]
-  [:.progress {:width "100%"
-               :max-width "800px"}])
+  [:.progress { :max-width "800px"}])
   
